0c20362ad8b9bb4268ece6d0c7b1543cddb338f0
Brok3nBass <35891882+Brok3nBass@users.noreply.github.com>
2018-04-14 04:23:43 +0200

    expainsion to stopwatch
    added something for if the game pauses (when it stops it will save that value until you go on with the game however many times you pause)

--- GameProject/src/gameproject/StopWatch.java
+++ GameProject/src/gameproject/StopWatch.java
@@ -1,46 +1,68 @@
 package gameproject;
 
+import java.util.ArrayList;
+
 public class StopWatch {
 
-  private long startTime = 0;
-  private long stopTime = 0;
-  private boolean running = false;
+    private long startTime = 0;
+    private long stopTime = 0;
+    private long beforePauseTime;
+    private boolean running = false;
+    private ArrayList<Long> pauseTimes;
 
-
-  public void start() {
-    this.startTime = System.currentTimeMillis();
-    this.running = true;
-  }
-
-
-  public void stop() {
-    this.stopTime = System.currentTimeMillis();
-    this.running = false;
-  }
-
-
-  //elaspsed time in milliseconds
-  public long getElapsedTime() {
-    long elapsed;
-    if (running) {
-      elapsed = (System.currentTimeMillis() - startTime);
-    } else {
-      elapsed = (stopTime - startTime);
+    public StopWatch() {
+        this.pauseTimes = new ArrayList<>();
     }
-    return elapsed;
-  }
 
-
-  //elaspsed time in seconds
-  public long getElapsedTimeSecs() {
-    long elapsed;
-    if (running) {
-      elapsed = ((System.currentTimeMillis() - startTime) / 1000);
-    } else {
-      elapsed = ((stopTime - startTime) / 1000);
+    public void start() {
+        if (this.startTime != 0) {
+            pauseTimes.add((System.currentTimeMillis() - startTime));
+            this.startTime = System.currentTimeMillis();
+        } else {
+            this.startTime = System.currentTimeMillis();
+        }
+        this.running = true;
     }
-    
-    return elapsed;
-  }
-  
+
+    public void stop() {
+        this.stopTime = System.currentTimeMillis();
+        this.running = false;
+    }
+
+    //elaspsed time in milliseconds
+    public long getElapsedTime() {
+        long elapsed;
+        beforePauseTime = checkPauseTime();
+        if (running) {
+            elapsed = (System.currentTimeMillis() - startTime);
+        } else {
+            elapsed = (stopTime - startTime);
+        }
+        return elapsed;
+    }
+
+    //elaspsed time in seconds
+    public long getElapsedTimeSecs() {
+        long elapsed;
+        beforePauseTime = checkPauseTime() / 1000;
+        if (running) {
+            elapsed = ((System.currentTimeMillis() - startTime) / 1000);
+        } else {
+            elapsed = ((stopTime - startTime) / 1000);
+        }
+
+        return elapsed + beforePauseTime;
+    }
+
+    private long checkPauseTime() {
+        if (!pauseTimes.isEmpty()) {
+            for (Long time : pauseTimes) {
+                beforePauseTime += time;
+            }
+            return beforePauseTime;
+        } else {
+            return 0;
+        }
+    }
+
 }
